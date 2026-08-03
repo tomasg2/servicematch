d245b2bb8630c4435ba9fd2d5344ce0f0cd4a44e
tomasg2 <158539410+tomasg2@users.noreply.github.com>
2026-08-03 22:15:46 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,44 @@
+// Service Worker for ServiceMatch.cz PWA
+const CACHE_NAME = 'servicematch-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './vop.html',
+  './css/styles.css',
+  './js/i18n.js',
+  './js/data.js',
+  './js/app.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
